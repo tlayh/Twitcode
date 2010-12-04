8809ffea985ee2998a5e48757adb2733221b70b6
Thomas Layh <thomas@layh.com>
2010-12-04 16:14:02 +0100

    added some missing changes

--- Classes/Domain/Repository/CodetypeRepository.php
+++ Classes/Domain/Repository/CodetypeRepository.php
@@ -36,6 +36,25 @@ class CodetypeRepository extends \F3\FLOW3\Persistence\Repository {
 	 */
 	protected $codeRepository;
 
+	/**
+	* overriding findAll method to get the oder of the codetypes
+	* @return Array of Codetypes
+	*/
+    public function findAll() {
+        $query = $this->createQuery();
+        $result = $query
+                ->matching($query->logicalNot($query->equals('name', '')))
+				->setOrderings(array('name' => \F3\FLOW3\Persistence\QueryInterface::ORDER_ASCENDING))
+                ->execute();
+        return $result->toArray();
+    }
+
+	/**
+	* get all codetypes that also have snippets available
+	* also return the count of snippets each codetype has
+	*
+	* @return array
+	*/
 	public function findAllWithSnippets() {
 		$query = $this->createQuery();
 		$codeTypes = $query
@@ -61,6 +80,12 @@ class CodetypeRepository extends \F3\FLOW3\Persistence\Repository {
 		 
 	}
 
+	/**
+	* currently only used for the importer
+	*
+	* @param $type
+	* @return array
+	*/
 	public function findByCodetype($type) {
 		$query = $this->createQuery();
 		$result = $query->matching($query->equals('type', $type))->setLimit(1)->execute();
