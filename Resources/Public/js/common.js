09ff9c4b7e8267f8c501055c5bed9622eeaf2e50
Thomas Layh <thomas@layh.com>
2010-12-04 14:42:17 +0100

    added confirm dialog for deleting your snippets

--- Resources/Public/js/common.js
+++ Resources/Public/js/common.js
@@ -2,7 +2,13 @@
 * activate tabs for textareas
 */
 jQuery(document).ready(function () {
+	// activate tabs for textarea
 	$("textarea#code").tabby();
+
+	// bind confirmation message on delete link
+	if($("#deletelink").length) {
+		$("#deletelink").click( function() { return confirm('Are you sure? This can not be undone!!'); } );
+	}
 });
 
 function showPopup() {
