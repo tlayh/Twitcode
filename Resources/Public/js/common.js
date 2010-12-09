c40d51d32428cd0f6bb3a2f6aa4fe031ec2639de
Thomas Layh <thomas@layh.com>
2010-12-09 18:06:47 +0100

    changed layout for lists and moved some settings

--- Resources/Public/js/common.js
+++ Resources/Public/js/common.js
@@ -6,9 +6,15 @@ jQuery(document).ready(function () {
 	$("textarea#code").tabby();
 
 	// bind confirmation message on delete link
-	if($("#deletelink").length) {
-		$("#deletelink").click( function() { return confirm('Are you sure? This can not be undone!!'); } );
+	if($(".deletelink").length) {
+		$(".deletelink").click( function() { return confirm('Are you sure? This can not be undone!!'); } );
 	}
+
+	// bind confirmation message on twitter link
+	if($(".twitterlink").length) {
+		$(".twitterlink").click( function() { return confirm('Are you sure you want to twitter your snippet again??'); } );
+	}
+	
 });
 
 function showPopup() {
