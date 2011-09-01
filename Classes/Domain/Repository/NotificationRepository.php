c0b1814274bd97e7068007d786770ae1f263e14d
tlayh <thomas@layh.com>
2011-09-01 10:08:04 +0200

    added notification repository and model

---
+++ Classes/Domain/Repository/NotificationRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace Layh\Twitcode\Domain\Repository;
+
+/**
+ * NotificationRepository
+ *
+ * Repository for Notifications
+ * @author Thomas Layh <develop@layh.com>
+ */
+
+class NotificationRepository extends \TYPO3\FLOW3\Persistence\Repository {
+
+}
