bff69bc12be8aee1150a1e42f7e20c91e0da4e04
tlayh <thomas@layh.com>
2011-09-05 11:22:49 +0200

    added model and repository for tag

---
+++ Classes/Domain/Model/Tag.php
@@ -0,0 +1,50 @@
+<?php
+namespace Layh\Twitcode\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Layh.Twitcode".              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        */
+
+/**
+ * Tag model for the Layh.Twitcode package
+ *
+ * @scope prototype
+ * @entity
+ *
+ * @author Thomas Layh <develop@layh.com>
+*/
+class Tag {
+
+	/**
+	 * @var string
+	 */
+	protected $title;
+
+	/**
+	 * @param string $title
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+}
---
+++ Classes/Domain/Repository/TagRepository.php
@@ -0,0 +1,15 @@
+<?php
+namespace Layh\Twitcode\Domain\Repository;
+
+/**
+ * TagRepository
+ *
+ * Repository for Tag
+ *
+ * @author Thomas Layh <develop@layh.com>
+ *
+ */
+
+class TagRepository extends \TYPO3\FLOW3\Persistence\Repository {
+
+}
