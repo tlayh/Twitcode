78c950a1e4c576aae847583043e956ab5fca4106
tlayh <thomas@layh.com>
2011-09-13 22:51:39 +0200

    added required sass files

---
+++ Resources/Public/twitcode/config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compressed
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
